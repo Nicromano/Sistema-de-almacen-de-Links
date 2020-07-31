03273e79948cef99a93c73ee43d5253e97349300
Nicromano <40124504+Nicromano@users.noreply.github.com>
2020-07-30 20:56:26 -0500

    sesiones lista

--- polls/urls.py
+++ polls/urls.py
@@ -6,14 +6,18 @@ Created on Wed Jul 29 21:52:59 2020
 """
 
 from django.urls import path
+from . import views
 
-from  . import views
 
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('createLink/', views.createLink, name="createLink"),
     path('editLink/<int:link_id>', views.editLink),
-    path('dashboard/<int:user_id>', views.dashboard, name="dashboard"),
-    path('addUser/', views.addUser, name="addUser"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    
+    
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,14 +6,20 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import User, Link
 from django.urls import reverse
 
+
 def index(request):
-    return render(request, 'polls/form_init.html')
+
+    try:
+        request.session['user']
+        return redirect('/dashboard')
+    except :
+        return render(request, 'polls/login.html')
     
-
-def addUser(request):
+def logout(request):
+    del request.session['user']
+    return redirect('/')
+def login(request):
     username_post = request.POST['username']
-    id = 0
-
     id = User.objects.filter(username = username_post).values_list('id', flat=True)
     if len(id) == 0:
         #No existe usuario
@@ -22,8 +28,12 @@ def addUser(request):
         id = user.id
     else:
         id = id[0]
-    
-    return HttpResponseRedirect(reverse('polls:dashboard', args=(id,)))
+
+    request.session['user'] = id
+    print(request.session['user'])
+    #return HttpResponseRedirect(reverse('polls:dashboard', args=(id,)))
+    return redirect('/dashboard')
+#
 
 
 def createLink(request):
@@ -31,9 +41,9 @@ def createLink(request):
     return  render(request, 'polls/dashborad')
     
 
-def dashboard(request, user_id):
-    print(user_id)
-    return HttpResponse('ver links')
+def dashboard(request):
+    
+    return HttpResponse(request.session['user'])
 
 def editLink(request, link_id):
     return HttpResponse('editar link')
